31a5b5d1237f03f9d560a23967916887409f9875
raphael malka <raphael.malka@student.vinci.be>
2023-12-10 22:40:01 +0100

    resolved query sql

--- api/app.js
+++ api/app.js
@@ -21,23 +21,30 @@ app.get('/', (req, res) => {
 // Route pour récupérer les logs
 app.get('/logs', async (req, res) => {
   try {
-    const { rows } = await pool.query('SELECT * FROM project.logs');
+    const { rows } = await pool.query('SELECT * FROM projet.logs');
     res.json(rows);
   } catch (err) {
     res.status(500).send('Erreur lors de la récupération des logs');
   }
 });
 
+
 // Route pour ajouter un log
 app.post('/logs', async (req, res) => {
-  const { user, date } = req.body;
-  try {
-    await pool.query('INSERT INTO project.logs (user, availability) VALUES ($1, $2)', [user, date]);
-    res.status(201).send('Log ajouté');
-  } catch (err) {
-    res.status(500).send('Erreur lors de l\'ajout du log');
-  }
-});
+    const { log } = req.body;  // Récupération du log depuis le corps de la requête
+    const date = new Date().toISOString();
+    console.log(log);
+    console.log(date);  
+  
+    try {
+      await pool.query('INSERT INTO projet.logs (log, date) VALUES ($1, $2)', [log, date]);
+      res.status(201).send('Log ajouté');
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Erreur lors de l\'ajout du log');
+    }
+  });
+  
 
 const PORT = 8080;
 app.listen(PORT, () => {
